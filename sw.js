168c2e67033a22bcba081983b80870d0799478fa
kitzzkitzz01 <kitzzkitzz91@gmail.com>
2026-09-04 17:58:48 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "rikis-os-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./study.html",
+    "./subject.html",
+    "./topic.html",
+    "./assignments.html",
+    "./exam.html",
+    "./progress.html",
+    "./code.html",
+    "./projects.html",
+    "./planner.html",
+    "./her.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
